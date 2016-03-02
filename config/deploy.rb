97ae702ddb7a8ce29e09cb4c79a2138dee9bb379
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-03-03 08:06:06 +0900

    add db:seed task

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,4 +59,14 @@ namespace :deploy do
     end
   end
 
+  desc 'db_seed'
+  task :db_seed do
+    on roles(:db) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
 end
